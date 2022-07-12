9bfbf512b26c412055c0875ced367f1d90f266b5
Lightiiy <gabrielbekier@gmail.com>
2022-07-12 21:05:37 +0200

    Style and buttons. Functionality WiP
    Working on buttons. There is an error with variable decleration, look into that next time

---
+++ index.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const sectBody = document.querySelectorAll(".main-content");
+
+function PageTrans() {
+    //asudn
+    for(let i = 0; i < sectBtn.length; i++)
+    {
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
+            this.className += 'active-button';
+        
+        })
+
+    }
+}
+
+PageTrans();
