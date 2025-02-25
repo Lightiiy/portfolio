00a1ad2c7b79782c76251d37cbe8d9334210383f
Gabriel Bekier <80338462+Lightiiy@users.noreply.github.com>
2025-02-25 22:37:40 +0100

    mouse follower

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    ngZoneEventCoalescing: true,
+  })
+  .catch((err) => console.error(err));
+
+document.addEventListener('mousemove', (event) => {
+  const follower = document.querySelector(
+    '.portfolioBackground__fancyCursor',
+  ) as HTMLElement;
+  if (follower) {
+    follower.style.left = `${event.pageX - 50}px`;
+    follower.style.top = `${event.pageY - 50}px`;
+  }
+});
