597e5133b26eab2c7173585eb4d4a35ae94f229d
Gabriel Bekier <80338462+Lightiiy@users.noreply.github.com>
2025-02-26 02:42:46 +0100

    work on image banner

--- src/app/components/home-page/home-page.module.ts
+++ src/app/components/home-page/home-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { SharedModule } from '../../shared/components/shared.module';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, BannerComponent],
   imports: [CommonModule, SharedModule],
   exports: [HomePageComponent],
 })
